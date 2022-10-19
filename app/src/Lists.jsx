4208170e4cb9be0102cd4940cd0bde7a5569950e
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-19 17:43:11 +0000

    list items render with info

--- app/src/Lists.jsx
+++ app/src/Lists.jsx
@@ -1,26 +1,20 @@
 export function SetList(props) {
 
-    // let items = [];
-    
-    // if(props.page === 'Home') {
-        
-    //     console.log('deez')
-
-    //     for (let i = 0; i < props.ToDoItem; i++) {
-    //         items.push(
-    //             <li>
-    //                 <p>{props.ToDoItem[i].INPUT}</p>
-    //             </li>
-    //         )
-    //     }
-    // }
-
     return(
         <>
             <div className="container-fluid">
-                <div className="row justify-content-center">
-                    {props.ToDoItem.map((item => <li>{item.input}</li>))}
-                </div>
+                {props.ToDoItem.map(((item, i) => 
+                                            <div className="row-10 justify-constent-center" id={item.list + i} key={item.list + i}>
+                                                <div className="col-6 d-flex">
+                                                    {item.input}
+                                                </div>
+                                                <div className='col-2'>
+                                                    <i className="bi bi-check-circle-fill align-self-end"></i>
+                                                    <i className="bi bi-x-circle-fill align-self-end"></i>
+                                                </div>
+                                            </div>
+                    ))
+                }
             </div>
         </>
     )
--- app/src/UserInput.jsx
+++ app/src/UserInput.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function UserInput(props) {
 
     const userInput = React.useRef(null);
-    const priority = React.useRef(null);
-    const list = React.useRef(null);
+    const prioritySelect = React.useRef(null);
+    const listSelect = React.useRef(null);
 
     function addItem() {
 
         const input = userInput.current.value;
-        const priority = priority.current.value;
-        const list = list.current.value;
+        const priority = prioritySelect.current.value;
+        const list = listSelect.current.value;
         const status = true;
 
         props.SetToDoItem([...props.ToDoItem, {input, priority, list, status}])
@@ -23,12 +23,12 @@ function UserInput(props) {
     return (
         <div className="col-10">
         <input ref={userInput}/>
-        <select name="priority" ref={priority}>
+        <select name="priority" ref={prioritySelect}>
             <option value="High">High</option>
             <option value="Med">Medium</option>
             <option value="Low">Low</option>
         </select>
-        <select name="list" ref={list}>
+        <select name="list" ref={listSelect}>
             <option value="default">To Do</option>
         </select>
         <i className="bi bi-plus-square-fill" onClick={() => {addItem()}}></i>
