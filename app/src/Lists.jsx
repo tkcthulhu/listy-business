79b2fc3688114073d5851507cba25a8cbe5a3018
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-19 18:43:19 +0000

    list now updates properly and buttons work, page no longer goes up in flames if you look at it funny

--- app/src/Lists.jsx
+++ app/src/Lists.jsx
@@ -1,20 +1,38 @@
 export function SetList(props) {
 
+    function setToDone(list, item) {
+        list[item].status = false
+    }
+
+    function deleteItem(list, item) {
+        list.splice(item, 1)
+        props.SetToDoItem(list)
+    }
+
+    let items = [];
+
+    const currentList = [...props.ToDoItem]
+
+    console.log(currentList)
+
+    for (let i = 0; i < currentList.length; i++) {
+        items.push(
+            <div className="row-10 justify-constent-center" id={currentList[i].list + i} key={currentList[i].list + i}>
+                <div className="col-6 d-flex">
+                    {currentList[i].input}
+                </div>
+                <div className='col-2'>
+                    <i className="bi bi-check-circle-fill align-self-end" onClick={() => setToDone(currentList, i)}></i>
+                    <i className="bi bi-x-circle-fill align-self-end" onClick={() => deleteItem(currentList, i)}></i>
+                </div>
+            </div>
+        )
+    }
+
     return(
         <>
             <div className="container-fluid">
-                {props.ToDoItem.map(((item, i) => 
-                                            <div className="row-10 justify-constent-center" id={item.list + i} key={item.list + i}>
-                                                <div className="col-6 d-flex">
-                                                    {item.input}
-                                                </div>
-                                                <div className='col-2'>
-                                                    <i className="bi bi-check-circle-fill align-self-end"></i>
-                                                    <i className="bi bi-x-circle-fill align-self-end"></i>
-                                                </div>
-                                            </div>
-                    ))
-                }
+                {items}
             </div>
         </>
     )
