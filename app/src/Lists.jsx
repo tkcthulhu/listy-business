def7b60af24eb3726d86654b202a30274e291734
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-20 23:59:30 +0000

    refactoring of buttons has been completed, too many for loops, but no further brain power for refactoring tonight

--- app/src/Lists.jsx
+++ app/src/Lists.jsx
@@ -8,14 +8,9 @@ export function SetList(props) {
 
     function setToActive(id, list) {
 
-        let butt = document.getElementById(id)
-
-        butt.classList.remove('done')
-
         for (let i = 0; i < list.length; i++) {
             if (list[i].id === id) {
-                console.log('This one officer')
-                list[i].status = false;
+                list[i].status = true;
             }
         }
         props.SetToDoItem(list)
@@ -23,15 +18,9 @@ export function SetList(props) {
     }
 
     function setToInactive(id, list) {
-
-        let butt = document.getElementById(id)
-
-        butt.classList.add('done')
-
         for (let i = 0; i < list.length; i++) {
             if (list[i].id === id) {
-                console.log('Take it back')
-                list[i].status = true;
+                list[i].status = false;
             }
         }
         props.SetToDoItem(list)
@@ -58,49 +47,31 @@ export function SetList(props) {
         localStorage.setItem('UserLists', JSON.stringify(remaining))
     }
 
-    let activeItems = [];
-
-    let inactiveItems = [];
-
     const currentList = [...props.ToDoItem]
 
-    function filterActive() {    
-        for (let i = 0; i < currentList.length; i++) {
-            if (currentList[i].status === true) {
-                activeItems.push(currentList[i])
-            }    
-        } return activeItems
+    function strikeThrough(status) {
+        if (status) {
+            return(
+                "row-10 justify-constent-center"
+            )
+        } else {
+            return(
+                "row-10 justify-constent-center done"
+            )
+        }
     }
-
-    // function filterInactive() {
-    //     for (let i = 0; i < currentList.length; i++) {
-    //         if (currentList[i].status === false) {
-    //             inactiveItems.push(
-    //                 <div className="row-10 justify-constent-center" id={currentList[i].id} key={currentList[i].id}>
-    //                     <div className="col-6 d-flex done">
-    //                         {currentList[i].input}
-    //                     </div>
-    //                     <div className='col-2'>
-    //                         <i className="bi bi-arrow-up-circle-fill align-self-end" onClick={() => setToInactive(currentList[i].id, currentList)}></i>
-    //                         <i className="bi bi-x-circle-fill align-self-end" onClick={() => deleteItem(currentList[i].id)}></i>
-    //                     </div>
-    //                 </div>
-    //             )    
-    //         }
-    //     } return inactiveItems
-    // }
 
     function statusButtons(id, status) {
 
-            if (status === false) {
+            if (status) {
 
                 return(
-                    <i className="bi bi-arrow-up-circle-fill align-self-end" onClick={() => setToInactive(id, currentList)}></i>
+                    <i className="bi bi-check-circle-fill align-self-end" onClick={() => setToInactive(id, currentList)}></i>
                 )
             } else {
 
                 return(
-                    <i className="bi bi-check-circle-fill align-self-end" onClick={() => setToActive(id, currentList)}></i>
+                    <i className="bi bi-arrow-up-circle-fill align-self-end" onClick={() => setToActive(id, currentList)}></i>
                 )
             } 
 
@@ -116,7 +87,7 @@ export function SetList(props) {
 
             let thisList = []
 
-            let completed = []
+            let incomplete = []
 
             let items = lists[i].listItems
 
@@ -124,13 +95,13 @@ export function SetList(props) {
 
             for (let i = 0; i < items.length; i++) {
 
-                if (!items[i].status) {
-                    completed.push('$')
+                if (items[i].status) {
+                    incomplete.push('$')
                     console.log('bloop')
                 }
 
                 thisList.push(
-                    <div className="row-10 justify-constent-center" id={items[i].id} key={items[i].id}>
+                    <div className={strikeThrough(items[i].status)} id={items[i].id} key={items[i].id}>
                         <div className="col-6 d-flex">
                             {items[i].input}
                         </div>
@@ -154,7 +125,7 @@ export function SetList(props) {
             userLists.push(
                 <>
                     <h4>
-                        {listName} : {completed.length} {deleteButton()}
+                        {listName} : {incomplete.length} {deleteButton()}
                     </h4>
                     {thisList}
                 </>
@@ -188,14 +159,10 @@ export function SetList(props) {
         }
     }    
 
-    filterActive();
-
     return(
         <>
             <div className="container-fluid">
                 {buildUserLists(filterItems())}
-                {/* <h4>MF DONE: {inactiveItems.length}</h4> */}
-                {/* {filterInactive()} */}
                 {removeGarbage()}
             </div>
         </>
